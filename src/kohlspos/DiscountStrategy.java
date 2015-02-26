52df7b89fae8d4fa7f0ad936efff649cc1ff6d57
darnell <darnell@L11115042797.wctc.msft>
2015-02-25 18:05:15 -0600

    First Commit

--- src/kohlspos/DiscountStrategy.java
+++ src/kohlspos/DiscountStrategy.java
@@ -1,18 +1,20 @@
 
 package kohlspos;
 
-public class DiscountStrategy {
+public interface DiscountStrategy {
 
-    public static void main(String[] args) {
-        
+      
         // Start Class
         // Get information from database
         // Get price for each item
         // Add item, quantity, discount, price, price after discount to line
         // Total everything
+    
+    double itemCost;
+        
+    public double getDiscount(double itemCost);
         
         
-        
-    }
+
     
 }
